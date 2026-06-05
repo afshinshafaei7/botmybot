aa6faa64d8d60f04fe2974b800cb5524000b5130
afshinshafaei7 <106693239+afshinshafaei7@users.noreply.github.com>
2026-06-05 19:18:54 +0330

    Fix token retrieval from environment variable

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 from save import save
 import os
-TOKEN = os.getenv("8384330394:AAEov4TaNmssTP0mcra_Y1HU-gmjDnf-K24")
+os.getenv("8384330394:AAEov4TaNmssTP0mcra_Y1HU-gmjDnf-K24")
 
 # ======= مشخصات تو =======
 my_info = {
